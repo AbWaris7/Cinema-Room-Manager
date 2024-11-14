bc4a4b5bf83df3dedd5146bfe650dfd1f89db248
abdulwariskhan <hellokhan.ch@gmail.com>
2024-11-14 22:08:44 +0100

    print seats

--- src/Cinema.java
+++ src/Cinema.java
@@ -1,2 +1,27 @@
 public class Cinema {
+
+    public static void main(String[] args) {
+
+        int rows = 7;
+        int columns =8;
+
+        System.out.println("Cinema:");
+        System.out.print(" ");
+
+        for(int i=1; i<=columns; i++) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        for (int i=1; i<=rows; i++) {
+            System.out.print(i);
+            for (int j=1; j<=columns; j++) {
+                System.out.print("S ");
+            }
+            System.out.println();
+        }
+
+
+
+
+    }
 }
