5d081989c85c23b08c5075b22151475eafba6401
abdulwariskhan <hellokhan.ch@gmail.com>
2024-12-10 11:20:14 +0100

    Set the ticket price

--- src/Cinema.java
+++ src/Cinema.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Cinema {
@@ -14,8 +15,12 @@ public class Cinema {
         System.out.println("Enter the number of seats in each row: ");
         int columns = input.nextInt();
 
+        String[][] tickets = new String[rows][columns];
+
+
         int totalSeats = rows * columns;
         int totalIncome = 0;
+        int singleTicketPrice = 0;
 
         if(totalSeats <60) {
             totalIncome = totalSeats * 10;
@@ -24,9 +29,57 @@ public class Cinema {
             int secondHalfRows = rows - firstHalfRows;
             totalIncome = (firstHalfRows * columns * PREMIUM_TICKET_PRICE) + (secondHalfRows * columns * DISCOUNT_TICKET_PRICE);
         }
-        System.out.println("Total income: ");
-        System.out.println("$"+totalIncome);
 
+        System.out.println("Cinema:");
+        for(int i = 1; i <=columns; i++) {
+            System.out.print(" "+i);
+        }
+        System.out.println();
+
+        for(int i = 1; i <=rows; i++) {
+            System.out.print(i);
+            for(int j = 1; j <=columns; j++) {
+                tickets[i-1][j-1] = "S";
+                System.out.print(" "+tickets[i-1][j-1]);
+            }
+            System.out.println();
+        }
+
+        System.out.println("Enter a row number:");
+        int BuyRow = input.nextInt();
+        System.out.println("Enter a seat number in that row:");
+        int BuyColumn = input.nextInt();
+
+        if(totalSeats <=60) {
+            singleTicketPrice = 10;
+        } else {
+
+            int frontHalfRows = rows / 2;
+            if (BuyRow <= frontHalfRows) {
+                singleTicketPrice = 10;  // Front half
+            } else {
+                singleTicketPrice = 8;   // Back half
+            }
+        }
+        System.out.println("Ticket price $"+singleTicketPrice);
+
+        tickets[BuyRow - 1][BuyColumn - 1] = "B";
+        System.out.println("Cinema:");
+        for(int i = 1; i <=columns; i++) {
+            System.out.print(" "+i);
+        }
+        System.out.println();
+
+        for(int i = 1; i <=rows; i++) {
+            System.out.print(i);
+            for(int j = 1; j <=columns; j++) {
+
+                System.out.print(" "+tickets[i-1][j-1] );
+
+            }
+
+            System.out.println();
+        }
 
     }
 }
