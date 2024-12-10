00d96a5dc2ca6aa105eb898e1db88cc386a1ef4f
abdulwariskhan <hellokhan.ch@gmail.com>
2024-12-10 08:00:13 +0100

    set the ticket price

--- src/Cinema.java
+++ src/Cinema.java
@@ -1,21 +1,32 @@
+import java.util.Scanner;
+
 public class Cinema {
 
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Magic Numbers
+        final int PREMIUM_TICKET_PRICE = 10;
+        final int DISCOUNT_TICKET_PRICE = 8;
 
-        System.out.println("Cinema:");
-        for(int row =1; row<9; row++){
-            System.out.print(" "+row);
-        }
+        System.out.println("Enter the number of rows: ");
+        int rows = input.nextInt();
+        System.out.println("Enter the number of seats in each row: ");
+        int columns = input.nextInt();
 
-        System.out.println();
-        for(int col =1; col<=7; col++){
-            System.out.print(col);
-            for(int row =1; row<9; row++){
-                System.out.print(" S");
-            }
-            System.out.println();
+        int totalSeats = rows * columns;
+        int totalIncome = 0;
+
+        if(totalSeats <60) {
+            totalIncome = totalSeats * 10;
+        } else {
+            int firstHalfRows = rows / 2;
+            int secondHalfRows = rows - firstHalfRows;
+            totalIncome = (firstHalfRows * columns * PREMIUM_TICKET_PRICE) + (secondHalfRows * columns * DISCOUNT_TICKET_PRICE);
         }
+        System.out.println("Total income: ");
+        System.out.println("$"+totalIncome);
+
 
     }
 }
